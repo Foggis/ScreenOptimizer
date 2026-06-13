ebd4bf73030584c9c39ea73ee7d9e4bb3cc03b31
Fogma <107138510+Foggis@users.noreply.github.com>
2026-06-13 05:18:35 +0200

    improved compatibility for 1.21.11

--- src/main/java/fogma/screenoptimizer/FabricEntrypoint.java
+++ src/main/java/fogma/screenoptimizer/FabricEntrypoint.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class FabricEntrypoint implements ModInitializer {
 	public static final String MOD_ID = "screenoptimizer";
-	public static final String VERSION = "1.0 (1.21.11 edition)";
+	public static final String VERSION = "1.1 (1.21.11 edition)";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
